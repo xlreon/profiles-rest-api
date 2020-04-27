fa95b702e11fbc24ced70cc8253f990a0f7475f5
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2020-04-27 19:06:56 +0530

    Viewset added with list action.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -48,3 +49,16 @@ class HelloApiView(APIView):
 
         return Response({'method': 'DELETE'})
 
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Return a hello message."""
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
