c3634897cc8bcbd9eb51742ccb0afe113500a2b7
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2020-04-27 20:40:53 +0530

    Token authentication added for UNSAFE_METHODS.

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -5,7 +5,7 @@ class UpdateOwnProfile(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile"""
-        if request.method in permissions.SAFE_METHOD:
+        if request.method in permissions.SAFE_METHODS:
             return True
         
         return obj.id == request.user.id
