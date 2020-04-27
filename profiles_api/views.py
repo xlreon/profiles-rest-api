5527e4816eb060c73274d5edcd465963dfe84ba8
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2020-04-27 18:49:41 +0530

    Put, patch & delete methods added.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -32,3 +32,19 @@ class HelloApiView(APIView):
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
                 )
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial update of object"""
+
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+
+        return Response({'method': 'DELETE'})
+
